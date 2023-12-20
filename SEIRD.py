61143d16169f098294639284102d0fded4a462aa
aliiavgh <aliyakomanovaa@gmail.com>
2023-12-20 22:32:14 +0600

    second

--- SEIRD.py
+++ SEIRD.py
@@ -3,17 +3,17 @@ from matplotlib import pyplot as plt
 from scipy.integrate import solve_ivp
 
 #SEIRD model base parameters
-beta = 0.1 # The probability of infection
-sigma = 0.2 # The speed of recovery: sigma = 1/T(the avarege duration of the disease)
-gamma = 0.1 # Incubation period 
-mu = 0.1 # Martality rate
+beta = 2.28 # The probability of infection
+sigma = 0.1 # The speed of recovery: sigma = 1/T(the avarege duration of the disease)
+gamma = 0.2 # Incubation period 
+mu = 0.034 # Martality rate
 
 #Time parameters 
 t_start = 0 
 t_end = 200
 t_interval = (t_start, t_end)
 
-#Initial coditions 
+# Initial coditions 
 S0 = 10 # Susceptible  
 E0 = 0.3 # Exposed 
 I0 = 0.0 # Infected 
@@ -21,7 +21,7 @@ R0 = 0.0 # Recovered
 D0 = 0.0 # Dead 
 
 
-# SEIRD model 
+# SEIRD model: Susceptible -> Exposed -> Infected -> Recovered / Dead
 def seird(t, y): 
     S, E, I, R, D = y 
     dSdt = -beta * S * I 
@@ -31,6 +31,7 @@ def seird(t, y):
     dDdt = mu * I
     return [dSdt, dEdt, dIdt, dRdt, dDdt]
 
+# The solution of the model 
 response = solve_ivp(seird, t_interval, [S0, E0, I0, R0, D0], t_eval=np.linspace(t_start, t_end, 1000))
 
 plt.figure(figsize=(12, 6))
--- animated_SEIR.py
+++ animated_SEIR.py
@@ -3,27 +3,26 @@ import matplotlib.animation as ani
 import numpy as np
 
 GREY = (0.78, 0.78, 0.78)  # Susceptible
-BLUE = (0, 0, 1)           #Exposed
 RED = (0.96, 0.15, 0.15)   # Infected
 GREEN = (0, 0.86, 0.03)    # Recovered
 BLACK = (0, 0, 0)          # Dead
 
 COVID19_PARAMS = {
-    "r0": 2.28,
+    "beta": 2.28,
     "incubation": 5,
     "percent_mild": 0.8,
     "mild_recovery": (7, 14),
     "percent_severe": 0.2,
     "severe_recovery": (21, 42),
     "severe_death": (14, 56),
-    "fatality_rate": 0.034,
+    "mu": 0.034,
     "serial_interval": 7
 }
 
-
-class Virus():
+#SEIR model: Susceptible -> Infected -> Recovered / Dead
+class seir():
     def __init__(self, params):
-        # create plot
+        # Create plot
         self.fig = plt.figure()
         self.axes = self.fig.add_subplot(111, projection="polar")
         self.axes.grid(False)
@@ -31,7 +30,7 @@ class Virus():
         self.axes.set_yticklabels([])
         self.axes.set_ylim(0, 1)
 
-        # create annotations
+        # Create annotations 
         self.day_text = self.axes.annotate(
             "Day 0", xy=[np.pi / 2, 1], ha="center", va="bottom")
         self.infected_text = self.axes.annotate(
@@ -41,16 +40,16 @@ class Virus():
         self.recovered_text = self.axes.annotate(
             "\n\nRecovered: 0", xy=[3 * np.pi / 2, 1], ha="center", va="top", color=GREEN)
 
-        # create member variables
+        # Create member variables
         self.day = 0
         self.total_num_infected = 0
         self.num_currently_infected = 0
         self.num_recovered = 0
         self.num_deaths = 0
-        self.r0 = params["r0"]
+        self.beta = params["beta"]
         self.percent_mild = params["percent_mild"]
         self.percent_severe = params["percent_severe"]
-        self.fatality_rate = params["fatality_rate"]
+        self.mu = params["mu"]
         self.serial_interval = params["serial_interval"]
 
         self.mild_fast = params["incubation"] + params["mild_recovery"][0]
@@ -80,7 +79,7 @@ class Virus():
         self.thetas = np.pi * (1 + 5**0.5) * indices
         self.rs = np.sqrt(indices / population)
         self.plot = self.axes.scatter(self.thetas, self.rs, s=5, color=GREY)
-        # patient zero
+        # Patient 0
         self.axes.scatter(self.thetas[0], self.rs[0], s=5, color=RED)
         self.mild[self.mild_fast]["thetas"].append(self.thetas[0])
         self.mild[self.mild_fast]["rs"].append(self.rs[0])
@@ -89,7 +88,7 @@ class Virus():
     def spread_virus(self, i):
         self.exposed_before = self.exposed_after
         if self.day % self.serial_interval == 0 and self.exposed_before < 4500:
-            self.num_new_infected = round(self.r0 * self.total_num_infected)
+            self.num_new_infected = round(self.beta * self.total_num_infected)
             self.exposed_after += round(self.num_new_infected * 1.1)
             if self.exposed_after > 4500:
                 self.num_new_infected = round((4500 - self.exposed_before) * 0.9)
@@ -120,7 +119,7 @@ class Virus():
                     self.one_by_one,
                     interval=50,
                     frames=len(thetas),
-                    fargs=(thetas, rs, BLUE))
+                    fargs=(thetas, rs, RED))
             self.assign_symptoms()
 
         self.day += 1
@@ -144,11 +143,9 @@ class Virus():
     def assign_symptoms(self):
         num_mild = round(self.percent_mild * self.num_new_infected)
         num_severe = round(self.percent_severe * self.num_new_infected)
-        # choose random subset of newly infected to have mild symptoms
         self.mild_indices = np.random.choice(self.new_infected_indices, num_mild, replace=False)
-        # assign the rest severe symptoms, either resulting in recovery or death
         remaining_indices = [i for i in self.new_infected_indices if i not in self.mild_indices]
-        percent_severe_recovery = 1 - (self.fatality_rate / self.percent_severe)
+        percent_severe_recovery = 1 - (self.mu / self.percent_severe)
         num_severe_recovery = round(percent_severe_recovery * num_severe)
         self.severe_indices = []
         self.death_indices = []
@@ -156,7 +153,6 @@ class Virus():
             self.severe_indices = np.random.choice(remaining_indices, num_severe_recovery, replace=False)
             self.death_indices = [i for i in remaining_indices if i not in self.severe_indices]
 
-        # assign recovery/death day
         low = self.day + self.mild_fast
         high = self.day + self.mild_slow
         for mild in self.mild_indices:
@@ -225,8 +221,8 @@ class Virus():
 
 
 def main():
-    coronavirus = Virus(COVID19_PARAMS)
-    coronavirus.animate()
+    pandemic = seir(COVID19_PARAMS)
+    pandemic.animate()
     plt.show()
 
 
